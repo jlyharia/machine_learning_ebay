a711b4dcee6817ea24fffc73d0a2f6122cc5f5f8
jlyharia <jlyhtech@gmail.com>
2018-05-08 00:36:03 -0700

    update

--- Homework_1/Part 1 (Linear Regression)/task2.r
+++ Homework_1/Part 1 (Linear Regression)/task2.r
@@ -9,6 +9,8 @@ colnames(cps_data) <- c('EDUCATION','SOUTH','SEX', 'EXPERIENCE','UNION', 'WAGE',
 
 
 y = cps_data['WAGE']
+X = cps_data[, c('EDUCATION','SOUTH','SEX', 'EXPERIENCE','UNION','AGE', 'RACE', 'OCCUPATION', 'SECTOR', 'MARR')]
+
 # normalizeMatrix(cps_data)
 # mean(mat)
 # normalizeVector(c(3,2,4))
@@ -18,10 +20,15 @@ y = cps_data['WAGE']
 # message("b. Fit a linear regression model, with the WAGE variable as the dependant variable
 # and the other variables as predictors. Report the R-squared and adjusted
 # R-squared metrics. Explain these metrics.")
+
 # message("c. What is the regression coefficient of the EDUCATION variable? How can this
 # coefficient be interpreted?")
+
 # message("d. What are the regression coefficients of all SECTOR related categorical variables?
 # How can these be interpreted?")
+
 # message("e. Observe the p-values of the regression coefficients. Does a higher p-value indicate more or less certainty of a regression coefficient?")
+
 # message("f. Report the AIC (Akaike Information Criteria) metric of the above model. This is another metric to evaluate the model fit, adjusting for model complexity. Lower values indicate a better fit.")
+
 # message("g. The residuals provide diagnostics about the model fit. For a well behaved model, the residuals should be Normally distributed with 0 mean and constant variance, and have no correlation with the predicted value. Plot the Residuals vs. the predicted value, as well as a Quantile-Quantile plot (QQplot) of the Residuals with respect to the standard Normal distribution.")
